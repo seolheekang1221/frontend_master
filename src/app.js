56c0f4fa564806cb157fcafac850a647deaa9ff0
seolhee <seolheekang1221@gmail.com>
2022-08-25 22:08:16 -0400

    change app.js

--- src/app.js
+++ src/app.js
@@ -11,21 +11,5 @@ const App = () => {
   )
 }
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
-};
 
 render(React.createElement(App), document.getElementById("root"));
